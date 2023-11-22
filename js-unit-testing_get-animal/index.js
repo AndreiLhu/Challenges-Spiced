6aef9d68694bf050609ff37645f420257a228a71
AndreiLhu <leahu.andreilucian@gmail.com>
2023-11-22 10:37:25 +0100

    testing get animal challenge done

---
+++ js-unit-testing_get-animal/index.js
@@ -0,0 +1,11 @@
+export function getAnimal(animal) {
+  if (animal === "cats") {
+    return `I totally love ${animal}!`;
+  }
+  if (typeof animal === "string") {
+    return `I like ${animal}!`;
+  }
+  if (typeof animal === "undefined") {
+    return "I do not like animals at all!";
+  }
+}
