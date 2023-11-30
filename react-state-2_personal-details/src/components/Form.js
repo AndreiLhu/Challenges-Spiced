1ac4f0c3e8c5306a991f3dd68ed5f5030058c153
AndreiLhu <leahu.andreilucian@gmail.com>
2023-11-30 11:37:29 +0100

    personal details challenge done

---
+++ react-state-2_personal-details/src/components/Form.js
@@ -0,0 +1,28 @@
+export default function Form({ onHandleCreateUser }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.elements.name.value;
+    const email = form.elements.email.value;
+    onHandleCreateUser(name, email);
+    form.reset();
+    form.elements.name.focus();
+  }
+
+  return (
+    <form
+      className="form"
+      aria-labelledby="user-details"
+      onSubmit={handleSubmit}
+    >
+      <h2 id="user-details">Please enter your details here!</h2>
+      <label htmlFor="name">Name: </label>
+      <input id="name" name="name" type="text" placeholder="John Doe" />
+      <label htmlFor="email">Email: </label>
+      <input id="email" name="email" type="email" placeholder="john@doe.com" />
+      <button className="form__submit-button" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+}
